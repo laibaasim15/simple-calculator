884131b9b830230b904e98ee1ceb16d87a70e4f4
laibaasim15 <mastersubhan15@gmail.com>
2024-04-07 02:00:54 +0500

    tweak the text and added console at the end

--- index.js
+++ index.js
@@ -2,25 +2,29 @@
 // SHBANG
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
-  {
-    message: "Select one of the operators to perfom action",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perfom action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
 ]);
 // conditional statement
 if (answer.operator === "Addition") {
-  console.log(answer.firstNumber + answer.secondNumber);
-} else if (answer.operator === "Subtraction") {
-  console.log(answer.firstNumber - answer.secondNumber);
-} else if (answer.operator === "Multiplication") {
-  console.log(answer.firstNumber * answer.secondNumber);
-} else if (answer.operator === "Division") {
-  console.log(answer.firstNumber / answer.secondNumber);
-} else {
-  console.log("Please select a valid operators");
+    console.log(answer.firstNumber + answer.secondNumber);
 }
-console.log(answer);
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select a valid operators");
+}
+console.log("THE END");
